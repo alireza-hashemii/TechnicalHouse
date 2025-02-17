ad5614414eb656f622c1c60476b0de8bfa4b2bd7
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-02-17 14:46:23 +0330

    7

--- sample_codes/d.py
+++ sample_codes/d.py
@@ -1,19 +1,14 @@
-class Contact:
-    contact_list : set["Contact"] = list()
-    
-    def __init__(self, name:str, email:str) -> None:
-        self.name = name
-        self.email = email
+# 1
+marks = [15,16,14,19]
+file = open('fg.txt','r+')
 
-        if not self in Contact.contact_list:
-            Contact.contact_list.append(self)
+split_content = file.readlines()
+for index, mark in enumerate(split_content):
+    split_content[index] += str(marks[index])
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.name}, {self.email})"
 
-cnt1 = Contact("Ali", "ali_000@gmail.com")
-cnt2 = Contact("Amir", "amir_000@gmail.com")
+f = " ".join(split_content).replace('\n','')
+print(f)
+file.write(f)
 
-print(cnt1.contact_list)
-print(set(Contact.contact_list))
-print(len(Contact.contact_list))
+# 2 counting words in a file
